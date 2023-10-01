a15f7b4d5729714a25369fa987a0b178f3ece380
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-10-01 09:51:32 -0500

    dialog

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -13,7 +13,7 @@ const DialogTrigger = DialogPrimitive.Trigger
 const DialogPortal = ({
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn()} {...props} />
+  <DialogPrimitive.Portal {...props} />
 )
 DialogPortal.displayName = DialogPrimitive.Portal.displayName
 
